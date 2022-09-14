6746632b5110149c08694542d3379abb6cec340e
Heri Hermawan <herhermawan007@gmail.com>
2022-09-14 08:33:49 +0700

    readme

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/'.request()->route()->parameter('locale')) }}">
+                <a class="navbar-brand" href="{{ '//'.env('CENTRAL_DOMAIN', 'localhost').'/'.request()->route()->parameter('locale') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
